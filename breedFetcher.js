b957f25a3ccf1121b1e599e062578cc60a3a2e33
Paul Formby <paulformby@icloud.com>
2023-10-21 03:08:30 +0000

    add tests for function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,29 @@
 const request = require("request");
 
-breed = process.argv[2];
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  (error, _response, body) => {
-    if (error) {
-      return console.log(error);
-    }
-    const data = JSON.parse(body);
+const fetchBreedDescription = function(breedName, callback) {
+  // Construct the URL
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-    if (data[0]) {
-      console.log(data[0].description);
+  request(url, (error, _response, body) => {
+    if (error) {
+      // If there's an error, pass the error as the first argument
+      // and null as the second argument to the callback
+      callback(error, null);
     } else {
-      console.log(`breed not found`);
+      // Parse the JSON response
+      const data = JSON.parse(body);
+
+      if (data[0]) {
+        // If the breed is found, pass null as the first argument
+        // and the description as the second argument to the callback
+        callback(null, data[0].description);
+      } else {
+        // If the breed is not found, pass an error message as the first argument
+        // and null as the second argument to the callback
+        callback("Breed not found", null);
+      }
     }
-  }
-);
+  });
+};
+
+module.exports = { fetchBreedDescription };
